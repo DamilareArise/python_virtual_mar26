ac9c1fbdccb8f00b0e97feaade5b55370be856b8
Arise Damilare <arisedamilare5@gmail.com>
2026-03-28 10:09:53 +0100

    week 2

---
+++ index.py
@@ -0,0 +1,72 @@
+# print(2 + 2)
+# commenting - 1. Debugging 2. Documentation
+'''
+how are you?
+I hope you are good?
+
+'''
+
+
+# 1. Indentation
+
+# print("My name is Damilare")
+
+# def sayhello():
+#     print("hello")
+    
+# sayhello()
+
+# 2. Variables 
+# bottle = "Water"
+name = "Samson"
+# name = "Tomiwa"
+
+# print(name)
+
+# The variable name
+# The assignment operator (=)
+# the value 
+
+# rules guiding variable declaration
+# 1. It can only start alphabets or underscore
+# _firstname = "Samson"
+# 2. It doesnt allow spaces, hence
+    # i. Camel casing : firstNameOfTheStudent
+    # ii. Snake casing: first_name_of_the_student
+    # iii Pascal casing: FirstNameOfTheStudent
+
+# 3. avoid using python dedicated syntax as a variable name 
+# 4. Variable name should be descriptive enough
+
+#    Types of variable declaration
+# 1. single variable, single values 
+bottle = "water"
+# 2. single variable, multiple value
+# bottle = "water", "soap", "oil"
+# print(bottle)
+# 3. multiple variable, single value 
+# x = y = z = 5
+# y = 20
+# print(y)
+# 4. Multiple variable, multiple value
+# x, y, z = 10, 20, 30
+# print(x)
+
+
+# concatenation 
+# print("Samson " + "Meshach")
+name = "Samson"
+age = 30
+# print("My name is "+name)
+# print("I am "+str(age)+" years old")
+
+# print("My name is", name)
+# print("I am",age,"years old")
+
+# f-string
+# print(f"My name is {name}. I am {age}years old")
+
+# name, course, occupation, age, 
+
+
+# 3. Python Datatypes
